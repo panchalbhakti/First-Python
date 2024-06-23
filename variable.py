b8a21d87aa611a1fbe377b55c37f51c1c5b7e4b1
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-23 10:18:32 +0530

    Operators

---
+++ variable.py
@@ -0,0 +1,30 @@
+a = 6
+b = 9
+
+#Comparison Operator
+print(a < b)
+print(a > b)
+print(a == b)
+
+d = 6
+c = 6
+print(a <= b)
+print(a >= b)
+
+#assissnment Operators
+num = 10
+num += 10
+
+print(num)
+
+#logical operator - NOT, AND, OR
+a = 30
+b = 50
+
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print(val1 and val2)
+print(val1 or val2)
