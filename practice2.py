779a0a2a5d804359aac852e1ed2d894c1b6913ea
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-23 18:57:16 +0530

    Practice questions

---
+++ practice2.py
@@ -0,0 +1,10 @@
+#writing a program to input user name and printing its length
+name = input("Enter Your name: ")
+length = len(name)
+print(length)
+
+
+#print the occurence of $ in the string 
+str = "I m $ and i m used in currence of $ in americe $"
+print(str)
+print ("Ocurrence of $ in the string is", str.count("$"))
--- str_func.py
+++ str_func.py
@@ -1,1 +1,17 @@
 #String Function
+
+# str = "I am a coder"
+# str.endswith("er")
+# str.capitalize()
+# str.replace(old, new)
+# str.find(word)
+# str.count(coder)
+
+str = "i am studying python"
+print(str.endswith("thon")) #returns true
+print(str.endswith("th")) #returns false
+print(str.capitalize())
+print(str.replace("python", "coding"))
+print(str.find("am"))
+print(str.find("python"))
+print(str.count("python")) #how many times the word exists in the sentence
