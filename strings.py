13fde484f9171c428fccd6da1ac4e71edb0e03d5
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-23 17:59:24 +0530

    Strings in python

---
+++ strings.py
@@ -0,0 +1,21 @@
+#string is data type that stores a sequence of characters
+
+#creating strings
+sen1 = "My name is Bhakti Panchal" #using double quotes
+sen2 = 'My name is Bhakti Panchal' #using single quotes
+sen3 = '''My name is Bhakti Panchal''' #using triple quotes
+sen4 = """My name is Bhakti Panchal""" #using triple quotes
+print(sen1)
+print(sen2)
+print(sen3)
+print(sen4)
+
+str1 = "Bhakti "
+str2 = "Panchal"
+str = str1 + str2 #Combining both the strings str1 and str2
+print(str)
+
+#escape sequence characters- gives formating
+#tab-space(\t), next-line(\n)
+str3 = "I am Bhakti Panchal.\nI am 18 years old.\nI am in second year of Btech Computer Science."
+print(str3)
