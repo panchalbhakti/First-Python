ab634e543f55d98c857c0c17eaf50577afd3d79e
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-23 18:08:26 +0530

    Index in python

---
+++ indexing.py
@@ -0,0 +1,20 @@
+#Indexing
+#B H A K T I   P A N C  H  A  L
+#0 1 2 3 4 5 6 7 8 9 10 11 12 13 - Index (Spaces are also count)
+
+str = "Bhakti Panchal"
+print(str)
+print(str[0]) #B
+print(str[1]) #H
+print(str[2]) #A
+print(str[3]) #K
+print(str[4]) #T
+print(str[5]) #I
+print(str[6]) #" "
+print(str[7]) #P
+print(str[8]) #A
+print(str[9]) #N
+print(str[10]) #C
+print(str[11]) #H
+print(str[12]) #A
+print(str[13]) #L
--- strings.py
+++ strings.py
@@ -10,12 +10,16 @@ print(sen2)
 print(sen3)
 print(sen4)
 
+#Concatenation - Combining two strings
 str1 = "Bhakti "
 str2 = "Panchal"
 str = str1 + str2 #Combining both the strings str1 and str2
 print(str)
+print(len(str))
 
 #escape sequence characters- gives formating
 #tab-space(\t), next-line(\n)
 str3 = "I am Bhakti Panchal.\nI am 18 years old.\nI am in second year of Btech Computer Science."
+str4 = "I am Bhakti Panchal.\tI am 18 years old.\tI am in second year of Btech Computer Science."
 print(str3)
+print(str4)
