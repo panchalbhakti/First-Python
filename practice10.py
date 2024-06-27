fffcf4b6ea283c16e0bd612ff38fad068f832b0a
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-27 16:16:57 +0530

    q

---
+++ practice10.py
@@ -0,0 +1,9 @@
+#write a program to print the length of list.
+cities = ["ahmedabad", "gandhinagar", "rajkot", "surat", "palanpur"]
+def list_len(list):
+    length = len(list)
+    print(length)
+
+list_len(cities) 
+
+#write a program to print 
