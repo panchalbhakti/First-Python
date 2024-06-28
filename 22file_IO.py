64bd2c6f4531c40a1023198938920c8544f6e6e4
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-28 15:50:27 +0530

    I/O

--- 22file_IO.py
+++ 22file_IO.py
@@ -4,3 +4,13 @@
 #Types of files:
 # 1. Text Files: .txt, .docx, .log, etc
 # 2. Binary Files: .mp4, .mov, .png, .jpeg, etc
+
+# Open, read and close file
+# we have to open a file before reading or writing
+# to open a file: f = open("file_name", "mode")  (file_name should be sample.txt, demo.docx) (mode should be r: read mode, w: write mode) (By default mode in python is read)
+
+f = open("23sample.txt", "r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
