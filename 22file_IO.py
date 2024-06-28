497c02f0ca487a6853eaa95871903c36663e68a4
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-28 15:36:51 +0530

    Files I/O

---
+++ 22file_IO.py
@@ -0,0 +1,6 @@
+#File I/O in Python
+#Python can be used to perform operations on file (read and write data).
+
+#Types of files:
+# 1. Text Files: .txt, .docx, .log, etc
+# 2. Binary Files: .mp4, .mov, .png, .jpeg, etc
